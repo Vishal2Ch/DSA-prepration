f2526edb7f80f626ef7db72aa1245c7a134213db
VISHAL <vishalchauhan1830@gmail.com>
2025-08-27 10:23:48 +0530

    this file contains about how to make a function.

--- src/calculator_new.java
+++ src/calculator_new.java
@@ -1,7 +1,43 @@
+
 import java.util.Scanner;
 
 public class calculator_new {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+        int ans =0;
+
+
+        while(true){
+            char op = in.next().trim().charAt(0);
+            System.out.println("Enter the operators");
+            if(op =='+' || op =='-' || op == '*' || op =='/' || op=='%'){
+                //taking two numbers from user
+                System.out.println("Enter the  first number");
+                int num1 = in.nextInt();
+                System.out.println("Enter the second number");
+                int num2 = in.nextInt();
+
+                if(op=='+'){
+                    ans=num1+num2;
+                }if(op=='-'){
+                    ans=num1-num2;
+                }if(op=='*'){
+                    ans=num1*num2;
+                }if(op=='/'){
+                    if (num2!=0) {
+//                        System.out.println("any number cannot be divided by zero");
+                        ans = num1 / num2;
+                    }
+                }if(op=='%'){
+                    ans=num1%num2;
+                }
+            }else if (op == 'x' || op == 'X'){
+                break;
+            }else {
+                System.out.println("Invalid operator");
+            }
+            System.out.println(ans);
+        }
+
     }
 }
