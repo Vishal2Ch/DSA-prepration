bf6ff49a429afd7ac5c4b1387f06921428ab6d4a
VISHAL <vishalchauhan1830@gmail.com>
2025-09-03 23:21:17 +0530

    these files get left behind

--- basics_loop.java
+++ basics_loop.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class basics_loop {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        int count = 1;
+//        while(count != 5){
+//            System.out.println("Hello World");
+//            System.out.println(count);
+//            count++;
+//        }
+
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("this is " +i+" time");
+//        }
+//        int num1 = in.nextInt();
+//        for(int num =0; num < num1; num++){
+//            System.out.println(num);
+//        }
+
+//        int num =1;
+//        while(num<=5)
+//        {
+//            System.out.println("Hello World");
+//            num++;
+//        }
+
+        int n = 1;
+        do{
+            System.out.println(n);
+            n++;
+        } while (n <= 6);{
+            System.out.println("this is "+n+" time");
+        }
 
     }
 }
