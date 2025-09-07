8888356a73a81ff033c5f58cd5946123463c63be
VISHAL <vishalchauhan1830@gmail.com>
2025-09-07 17:02:57 +0530

    added files

--- CyclicSort/MissingPostiviveNo.java
+++ CyclicSort/MissingPostiviveNo.java
@@ -1,15 +1,17 @@
 package CyclicSort;
 
-public class MissingNoQues {
+public class MissingPostiviveNo {
     public static void main(String[] args) {
-        int[] arr={0,1,2,4,5,6};
-        System.out.println(MissingNumber(arr));
+
+
     }
-    static int MissingNumber(int[] arr){
+
+    static int FristMissingPostive(int[] arr){
         int i =0;
+        int correct = arr[i]-1;
         while(i<arr.length){
-            if (arr[i]<arr.length && arr[i]!=arr[arr[i]]){
-                swap(arr,i,arr[i]);
+            if (arr[i]>0 && arr[i]<=arr.length && arr[i]!=arr[correct]){
+                swap(arr,i,correct);
             }else{
                 i++;
             }
@@ -17,11 +19,11 @@ public class MissingNoQues {
 
         //search for first missing number
         for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index){
-                return index;
+            if (arr[index] != index +1){
+                return index+1;
             }
         }
-        return arr.length;
+        return arr.length+1;
     }
 
     static void swap(int[] arr, int first, int second){
